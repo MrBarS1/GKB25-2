67117b1ea5719093824035964c97927d5d97531d
MrBarS1 <46891200+MrBarS1@users.noreply.github.com>
2023-03-30 02:01:15 +0700

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,272 @@
+
+var url_main ='Main.html';
+
+var url_site = 'site.html';
+var url_navigation = 'site.html';
+var url_for_patient = 'for_pacient.html';
+var url_norm_documen = 'doki.html';
+var url_contact = 'cont.html';
+var url_mkc = 'MKC.html';
+var url_corruption = 'Corrupt.html';
+
+var url_lic1 = 'licenses_first_page.html';
+var url_lic2 = 'licenses_second_page.html';
+var url_lic3 = 'licenses_third_page.html';
+var url_lic4 = 'licenses_four_page.html';
+var url_lic5 = 'licenses_fifth_page.html';
+var url_lic6 = 'licenses_six_page.html';
+var url_lic7 = 'licenses_seven_page.html';
+var url_lic8 = 'licenses_eight_page.html';
+var url_lic9 = 'licenses_nine_page.html';
+var url_lic10 = 'licenses_ten_page.html';
+
+var url_svid = 'svidetel.html'
+var url_help = 'help.html'
+var url_adresa ='adresa.html'
+var url_rabota ='rabota.html'
+var url_pass = 'pass.html'
+var url_usl = 'usl.html'
+var url_prava = 'prava.html'
+var url_nomera = 'nomera.html'
+var url_categ = 'category.html'
+var url_pere2 = 'pere2.html'
+var url_pere2_2 = 'pere2_2.html'
+var url_pere3 = 'pere3.html'
+var url_pere3_2 = 'pere3_2.html'
+var url_pere3_3 = 'pere3_3.html'
+var url_pere3_4 = 'pere3_4.html'
+var url_pere3_5 = 'pere3_5.html'
+var url_pere3_6 = 'pere3_6.html'
+var url_pere3_7 = 'pere3_7.html'
+var url_pere3_8 = 'pere3_8.html'
+var url_pere3_9 = 'pere3_9.html'
+var url_pere3_10 = 'pere3_10.html'
+var url_pere3_11 = 'pere3_11.html'
+var url_pere3_12 = 'pere3_12.html'
+var url_pere3_13 = 'pere3_13.html'
+var url_pere3_14 = 'pere3_14.html'
+var url_vid = 'vid.html'
+
+var url_ter1 = 'territorial_first_page.html'
+var url_ter2 = 'territorial_second_page.html'
+var url_ter3 = 'territorial_third_page.html'
+var url_ter4 = 'territorial_four_page.html'
+var url_ter5 = 'territorial_fifth_page.html'
+var url_ter6 = 'territorial_six_page.html'
+var url_ter7 = 'territorial_seven_page.html'
+var url_ter8 = 'territorial_eight_page.html'
+var url_ter9 = 'territorial_nine_page.html'
+var url_ter10 = 'territorial_ten_page.html'
+var url_ter11 = 'territorial_eleven_page.html'
+var url_ter12 = 'territorial_tvelve_page.html'
+
+var url_OMC_1P ='OMC_first_page.html';
+var url_OMC_2P ='OMC_second_page.html';
+var url_OMC_3P ='OMC_third_page.html';
+var url_OMC_4P ='OMC_four_page.html';
+
+
+function OPEN_INDEX(id){
+    if(id === 'site'){
+        window.location.href = url_site;
+    }
+    else if(id === 'navigation'){
+        window.location.href = url_navigation;
+    }
+    else if(id === 'for_patient'){
+        window.location.href = url_for_patient;
+    }
+    else if(id === 'norm_document'){
+        window.location.href = url_norm_documen;
+    }
+    else if(id === 'omc'){
+        window.location.href = url_OMC_1P;
+    }
+    else if(id === 'contact'){
+        window.location.href = url_contact;
+    }
+    else if(id === 'mkc'){
+        window.location.href = url_mkc;
+    }
+    else if(id === 'corruption'){
+        window.location.href = url_corruption;
+    }
+    else if(id === 'main'){
+        window.location.href = url_main;
+    }
+    else if(id === 'OMC_2P'){
+        window.location.href = url_OMC_2P;
+    }
+    else if(id === 'OMC_3P'){
+        window.location.href = url_OMC_3P;
+    }
+    else if(id === 'OMC_4'){
+        window.location.href = url_OMC_4P;
+    }
+    else if(id === 'doki'){
+        window.location.href = url_lic1;
+    }
+    else if(id === 'lic1'){
+        window.location.href = url_lic2;
+    }
+    else if(id === 'lic2'){
+        window.location.href = url_lic3;
+    }
+    else if(id === 'lic3'){
+        window.location.href = url_lic4;
+    }
+    else if(id === 'lic4'){
+        window.location.href = url_lic5;
+    }
+    else if(id === 'lic5'){
+        window.location.href = url_lic6;
+    }
+    else if(id === 'lic6'){
+        window.location.href = url_lic7;
+    }
+    else if(id === 'lic7'){
+        window.location.href = url_lic8;
+    }
+    else if(id === 'lic8'){
+        window.location.href = url_lic9;
+    }
+    else if(id === 'lic9'){
+        window.location.href = url_lic10;
+    }
+    else if(id === 'lic10'){
+        window.location.href = url_lic9;
+    }
+    
+    else if(id === 'ter1'){
+        window.location.href = url_ter2;
+    }
+    else if(id === 'ter2'){
+        window.location.href = url_ter3;
+    }
+    else if(id === 'ter3'){
+        window.location.href = url_ter4;
+    }
+    else if(id === 'ter4'){
+        window.location.href = url_ter5;
+    }
+    else if(id === 'ter5'){
+        window.location.href = url_ter6;
+    }
+    else if(id === 'ter6'){
+        window.location.href = url_ter7;
+    }
+    else if(id === 'ter7'){
+        window.location.href = url_ter8;
+    }
+    else if(id === 'ter8'){
+        window.location.href = url_ter9;
+    }
+    else if(id === 'ter9'){
+        window.location.href = url_ter10;
+    }
+    else if(id === 'ter10'){
+        window.location.href = url_ter11;
+    }
+    else if(id === 'ter11'){
+        window.location.href = url_ter12;
+    }
+
+    else if(id === 'b1'){
+        window.location.href = url_svid;
+    }
+    else if(id === 'b2'){
+        window.location.href = url_ter1;
+    }
+    else if(id === 'b3'){
+        window.location.href = url_help;
+    }
+    else if(id === 'b4'){
+        window.location.href = url_usl;
+    }
+    else if(id === 'b5'){
+        window.location.href = url_pere;
+    }
+    else if(id === 'b6'){
+        window.location.href = url_pere2;
+    }
+    else if(id === 'pere2'){
+        window.location.href = url_pere2_2;
+    }
+    else if(id === 'b7'){
+        window.location.href = url_pere3;
+    }
+    else if(id === 'b8'){
+        window.location.href = url_adresa;
+    }
+    else if(id === 'b9'){
+        window.location.href = url_rabota;
+    }
+    else if(id === 'b10'){
+        window.location.href = url_pass;
+    }
+    else if(id === 'b11'){
+        window.location.href = url_prava;
+    }
+    else if(id === 'b12'){
+        window.location.href = url_nomera;
+    }
+    else if(id === 'b13'){
+        window.location.href = url_vid;
+    }
+    else if(id === 'b14'){
+        window.location.href = url_categ;
+    }
+
+    else if(id === 'omc1'){
+        window.location.href = url_OMC_2P;
+    }
+    else if(id === 'omc2'){
+        window.location.href = url_OMC_3P;
+    }
+    else if(id === 'omc3'){
+        window.location.href = url_OMC_4P;
+    }
+
+
+    else if(id === 'pere3'){
+        window.location.href = url_pere3_2;
+    }
+    else if(id === 'pere3_2'){
+        window.location.href = url_pere3_3;
+    }
+    else if(id === 'pere3_3'){
+        window.location.href = url_pere3_4;
+    }
+    else if(id === 'pere3_4'){
+        window.location.href = url_pere3_5;
+    }
+    else if(id === 'pere3_5'){
+        window.location.href = url_pere3_6;
+    }
+    else if(id === 'pere3_6'){
+        window.location.href = url_pere3_7;
+    }
+    else if(id === 'pere3_7'){
+        window.location.href = url_pere3_8;
+    }
+    else if(id === 'pere3_8'){
+        window.location.href = url_pere3_9;
+    }
+    else if(id === 'pere3_9'){
+        window.location.href = url_pere3_10;
+    }
+    else if(id === 'pere3_10'){
+        window.location.href = url_pere3_11;
+    }
+    else if(id === 'pere3_11'){
+        window.location.href = url_pere3_12;
+    }
+    else if(id === 'pere3_12'){
+        window.location.href = url_pere3_13;
+    }
+    else if(id === 'pere3_13'){
+        window.location.href = url_pere3_14;
+    }
+}
+
+
